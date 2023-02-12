86708769cabdfce5dc8542569c49b0f64600bbb8
Yasas770 <64302398+Yasas770@users.noreply.github.com>
2023-02-12 23:05:01 +0530

    added a comment

--- src/main.tsx
+++ src/main.tsx
@@ -14,22 +14,19 @@ import "./main.css"
 
 export default function main() {
   return (
+    // yassa
     <div className='main-dev'>
                
-                  <div id="Home"><Nav/></div>
-            
-                  <div className='mt-5'><Rarity/></div>
-                  <div id="Text"className='mt-5'><Text/></div>
-                  <div className='mt-5'id="Mint"><App/></div>
-                
+                <div id="Home"><Nav/></div>
+                <div className='mt-5'><Rarity/></div>
+                <div id="Text"className='mt-5'><Text/></div>
+                <div className='mt-5'id="Mint"><App/></div>
                 <div className='mt-5' id="Video"><Video/></div>
                 <div id="About"><About/></div>
                 <div id="Roadmap"><RoadMap/></div>
                 <div id="Team"className='mt-5'><Team/></div>
                 <div id="Faq" className='mt-5'><Faq/></div>
                 <div className='mt-5'><Footer/></div>
-               
-
-    </div>
+        </div>
   )
 }
